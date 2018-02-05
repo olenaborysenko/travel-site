1bd4c1fddd8362449571d3210e6c25d1dc3aec29
Olena <31489876+olenaborysenko@users.noreply.github.com>
2018-02-05 19:11:05 +0200

    Added BrowserSync package and auto-reload

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ postcss = require("gulp-postcss"),
 autoprefixer = require("autoprefixer"),
 cssVars = require("postcss-simple-vars"),
 nested = require("postcss-nested"),
-cssImport = require("postcss-import");
+cssImport = require("postcss-import"),
+browserSync = require("browser-sync").create();
 
 gulp.task("styles", function() {
 	return gulp.src("./app/assets/css/style.css")
@@ -13,7 +14,24 @@ gulp.task("styles", function() {
 });
 
 gulp.task("watch", function() {
-	watch("./app/assets/css/**/*.css", function() {
-		gulp.start("styles");
+
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
 	});
+
+	watch("./app/index.html", function() {
+		browserSync.reload();
+	});
+
+	watch("./app/assets/css/**/*.css", function() {
+		gulp.start("cssInject");
+	});
+});
+
+gulp.task("cssInject", ["styles"], function() {
+	return gulp.src("./app/temp/css/style.css")
+		.pipe(browserSync.stream());
 });
