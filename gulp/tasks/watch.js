d0cd849cc12e0511c83a72d2b5ee180c4d812bc7
Olena <31489876+olenaborysenko@users.noreply.github.com>
2018-02-05 19:23:50 +0200

    Organized gulpfile.js and added error handling

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,26 @@
+var gulp = require("gulp"),
+watch = require("gulp-watch"),
+browserSync = require("browser-sync").create();
+
+gulp.task("watch", function() {
+
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
+	});
+
+	watch("./app/index.html", function() {
+		browserSync.reload();
+	});
+
+	watch("./app/assets/css/**/*.css", function() {
+		gulp.start("cssInject");
+	});
+});
+
+gulp.task("cssInject", ["styles"], function() {
+	return gulp.src("./app/temp/css/style.css")
+		.pipe(browserSync.stream());
+});
