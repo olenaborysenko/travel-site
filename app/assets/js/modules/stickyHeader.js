1489a6bbfc33d527cc0b38028cb42a052639a0a4
Olena <31489876+olenaborysenko@users.noreply.github.com>
2018-02-07 01:20:15 +0200

    Added current section highlight and smooth scrolling from nav

--- app/assets/js/modules/stickyHeader.js
+++ app/assets/js/modules/stickyHeader.js
@@ -1,11 +1,21 @@
 import $ from "jquery";
 import waypoints from "../../../../node_modules/waypoints/lib/noframework.waypoints";
+import smoothScroll from "jquery-smooth-scroll";
 
 class StickyHeader {
 	constructor() {
 		this.siteHeader = $(".header");
 		this.headerTriggerElement = $(".large-hero__title");
 		this.createHeaderWaypoint();
+		this.pageSections = $(".page-section");
+		this.headerLinks = $(".primary-nav a");
+		this.largeHero = $(".large-hero");
+		this.createPageSectionWaypoints();
+		this.addSmoothScrolling();
+	}
+
+	addSmoothScrolling() {
+		this.headerLinks.smoothScroll();
 	}
 
 	createHeaderWaypoint() {
@@ -19,6 +29,36 @@ class StickyHeader {
 					that.siteHeader.removeClass("header--dark");
 				}
 			}
+		});
+	}
+
+	createPageSectionWaypoints() {
+		var that = this;
+		this.pageSections.each(function() {
+			var currentPageSection = this;
+			new Waypoint({
+				element: currentPageSection,
+				handler: function(direction) {
+					if (direction == "down") {
+						var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+						that.headerLinks.removeClass("is-current-link");
+						$(matchingHeaderLink).addClass("is-current-link");
+					}
+				},
+				offset: "20%"
+			});
+
+			new Waypoint({
+				element: currentPageSection,
+				handler: function(direction) {
+					if (direction == "up") {
+						var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+						that.headerLinks.removeClass("is-current-link");
+						$(matchingHeaderLink).addClass("is-current-link");
+					}
+				},
+				offset: "-80%"
+			});
 		});
 	}
 }
